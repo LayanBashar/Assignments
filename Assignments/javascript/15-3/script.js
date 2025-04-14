1d37bb4f14ca27500634a4b2dd218ce90d2dd9db
Layan <Layanbashar4@gmail.com>
2025-04-14 19:34:45 +0300

    newBranch

--- Assignments/javascript/15-3/script.js
+++ Assignments/javascript/15-3/script.js
@@ -1,67 +1,80 @@
-// length
-let tweet =
-  "javaScript is a versatile programming language used primarily for creating dynamic and interactive web pages. It enables developers to manipulate HTML and CSS, handle events, and communicate with servers. With frameworks like React and Vue, JavaScript plays a crucial role in modern web development.";
-length;
-if (tweet.length > 280) console.log("tweet exceeds 280 characters");
-else console.log("tweet does NOT exceeds 280 characters");
+// // for in
+// let mycar = {
+//   color: "black",
+//   model: "tesla",
+//   price: 100000,
+// };
 
-// charAt()
-let userName = "layan";
-userName = userName.charAt(0).toUpperCase() + userName.slice(1);
-console.log(userName);
-//toUpperCase()  toLowerCase()
-let title = "Web development";
-title = title.charAt(0).toUpperCase() + title.slice(1).toLowerCase();
-console.log(title);
+// for (let prop in mycar) {
+//   if (Object.prototype.hasOwnProperty.call(mycar, prop)) {
+//     console.log(prop + ": " + mycar[prop]);
+//   }
+// }
 
-// trim()
-let email = "  username@gmail.com  ";
-email = email.trim();
-console.log(email);
+// // length
+// let tweet =
+//   "javaScript is a versatile programming language used primarily for creating dynamic and interactive web pages. It enables developers to manipulate HTML and CSS, handle events, and communicate with servers. With frameworks like React and Vue, JavaScript plays a crucial role in modern web development.";
+// length;
+// if (tweet.length > 280) console.log("tweet exceeds 280 characters");
+// else console.log("tweet does NOT exceeds 280 characters");
 
-// slice()
-let article =
-  "The Importance of Programming Programming is a key skill driving technological advancement today. It allows the development of software, websites, and apps that make our lives easier.";
-article = article.slice(0, 100) + ".....Read more";
-console.log(article);
+// // charAt()
+// let userName = "layan";
+// userName = userName.charAt(0).toUpperCase() + userName.slice(1);
+// console.log(userName);
+// //toUpperCase()  toLowerCase()
+// let title = "Web development";
+// title = title.charAt(0).toUpperCase() + title.slice(1).toLowerCase();
+// console.log(title);
 
-// substring()
-let number = "0791231230";
-number = "*".repeat(6) + number.substring(6);
-console.log(number);
+// // trim()
+// let email = "  username@gmail.com  ";
+// email = email.trim();
+// console.log(email);
 
-// replace() or replaceAll()
-let word = " Hello How are you badword badword";
-word = word.replaceAll("badword", "******");
-console.log(word);
+// // slice()
+// let article =
+//   "The Importance of Programming Programming is a key skill driving technological advancement today. It allows the development of software, websites, and apps that make our lives easier.";
+// article = article.slice(0, 100) + ".....Read more";
+// console.log(article);
 
-// split()
-let comment = "user comment";
-console.log(comment.split(" "));
+// // substring()
+// let number = "0791231230";
+// number = "*".repeat(6) + number.substring(6);
+// console.log(number);
 
-// includes()
-let comments = "comment restrictedword comment comment ";
-if (comments.includes("restrictedword"))
-  console.log("Your message contains restricted words");
-else console.log(comments);
+// // replace() or replaceAll()
+// let word = " Hello How are you badword badword";
+// word = word.replaceAll("badword", "******");
+// console.log(word);
 
-// startsWith()  endsWith()
-let image = "picture.jpeg";
-if (image.endsWith(".png") || image.endsWith(".jpg") || image.endsWith(".jpeg"))
-  console.log("verified");
-else console.log("Not verified");
+// // split()
+// let comment = "user comment";
+// console.log(comment.split(" "));
 
-// repeat()
-let divider = "-";
-console.log(divider.repeat(40));
+// // includes()
+// let comments = "comment restrictedword comment comment ";
+// if (comments.includes("restrictedword"))
+//   console.log("Your message contains restricted words");
+// else console.log(comments);
 
-// concat()
-let t = "Web";
-t = t.concat(" Development");
-console.log(t);
+// // startsWith()  endsWith()
+// let image = "picture.jpeg";
+// if (image.endsWith(".png") || image.endsWith(".jpg") || image.endsWith(".jpeg"))
+//   console.log("verified");
+// else console.log("Not verified");
 
-// indexOf() lastindexOf()
-let p =
-  "JavaScript is a powerful language. Many developers use JavaScript for both front-end and back-end development. JavaScript is essential for creating dynamic websites.";
-console.log(p.indexOf("JavaScript"));
-console.log(p.lastIndexOf("JavaScript"));
+// // repeat()
+// let divider = "-";
+// console.log(divider.repeat(40));
+
+// // concat()
+// let t = "Web";
+// t = t.concat(" Development");
+// console.log(t);
+
+// // indexOf() lastindexOf()
+// let p =
+//   "JavaScript is a powerful language. Many developers use JavaScript for both front-end and back-end development. JavaScript is essential for creating dynamic websites.";
+// console.log(p.indexOf("JavaScript"));
+// console.log(p.lastIndexOf("JavaScript"));
