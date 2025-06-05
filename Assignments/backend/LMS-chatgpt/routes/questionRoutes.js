f1dbfcadd1e6fbf5e0a018da94301097672ddbcd
Layan <Layanbashar4@gmail.com>
2025-06-05 12:45:52 +0300

    commit

--- Assignments/backend/LMS-chatgpt/routes/questionRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import QuestionController from "../controllers/questionController.js";
-
-const router = express.Router();
-
-router.post("/", QuestionController.addQuestion);
-router.get("/:quiz_id", QuestionController.getQuizQuestions);
-
-export default router;
--- Assignments/backend/LMS/server.js
+++
@@ -1,9 +0,0 @@
-import app from "./app.js";
-import dotenv from "dotenv";
-dotenv.config();
-
-const PORT = process.env.PORT || "5000";
-const ENV = process.env.NODE_ENV || "development";
-
-app.listen(PORT, () => {});
-console.log(`Server is running in ${ENV} mode on port ${PORT}`);
